e8502fad2cb9383fca2d010aeb8c0c0c61feff77
Fred Monroe <fred@313v.com>
2019-01-09 15:00:50 -0800

    movies_001 dataset script

---
+++ uri/scripts/datasets/movies_001.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+import shutil
+import random
+
+data_path = Path('/scratch/bpho')
+sources = data_path/'datasources'
+src_1 = sources/'live_neuron_mito_timelapse_for_deep_learning'
+src_2 = sources/'MitoTracker_Red_FM_movie_data'
+
+datasets = data_path/'datasets'
+movies_001 = datasets/'movies_001'
+train = movies_001/'train'
+valid = movies_001/'valid'
+test = movies_001/'test'
+
+if movies_001.exists(): shutil.rmtree(movies_001)
+for fld in [train, valid, test]: fld.mkdir(parents=True)
+
+two_channel = list(src_1.glob('*MTGreen*.czi'))
+one_channel = [x for x in src_1.glob('*.czi') if x not in two_channel]
+
+test_mito = list(src_2.glob('*230*.czi'))
+train_mito = list(src_2.glob('*920*.czi'))
+
+valid_pct = 0.20
+train_files = []
+valid_files = []
+test_files = []
+
+random.seed(42)
+for lst in [two_channel, one_channel, train_mito]:
+    lst.sort()
+    random.shuffle(lst)
+    split_idx = int(valid_pct * len(lst))
+    valid_files += lst[-split_idx:]
+    train_files += lst[:-split_idx]
+
+test_files = test_mito
+
+for x in train_files: shutil.copy(x, train/x.name)
+for x in valid_files: shutil.copy(x, valid/x.name)
+for x in test_files: shutil.copy(x, test/x.name)
+

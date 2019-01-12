9ee0ac992941d0cff54fe0519e8fda5838a04863
Fred Monroe <fred@313v.com>
2019-01-12 16:30:51 -0500

    add full tiffs from czi, make dataset w/ just mitotracker

--- uri/scripts/datasets/helpers.py
+++ uri/scripts/datasets/helpers.py
@@ -25,7 +25,8 @@ def build_index(axes, ix_select):
     idx = [ix_select.get(ax, 0) for ax in axes]
     return tuple(idx)
 
-def czi_to_tiles(czi_fn, hr_ROI_dir, lr_ROI_dir, lr_ROI_small_dir, size=256, channels=None, depths=None, num_tiles=5, scale=4):
+def czi_to_tiles(czi_fn, hr_ROI_dir, lr_ROI_dir, lr_ROI_small_dir, 
+                 size=256, channels=None, depths=None, num_tiles=5, scale=4, max_scale=1.5, max_per_movie=True):
     #import pdb; pdb.set_trace()
     hr_ROI_dir, lr_ROI_dir, lr_ROI_small_dir= Path(hr_ROI_dir), Path(lr_ROI_dir), Path(lr_ROI_small_dir)
     hr_ROI_dir.mkdir(parents=True, exist_ok=True)
@@ -44,7 +45,7 @@ def czi_to_tiles(czi_fn, hr_ROI_dir, lr_ROI_dir, lr_ROI_small_dir, size=256, cha
                 for time_col in range(times):
                     idx = build_index(proc_axes, {'T': time_col, 'C': channel, 'Z':depth, 'X':slice(0,x),'Y':slice(0,y)})
                     img = data[idx].astype(np.float)
-                    if img_max is None: img_max = img.max() * 1.5
+                    if img_max is None: img_max = img.max() * max_scale
                     img /= img_max
                     pimg = PIL.Image.fromarray((img*255).astype(np.uint8), mode='L')
                     rc = vision.transforms.RandomCrop([size, size])
@@ -62,8 +63,38 @@ def czi_to_tiles(czi_fn, hr_ROI_dir, lr_ROI_dir, lr_ROI_small_dir, size=256, cha
                             count = count+1
                             ROI.save(save_fn)
                             cur_size = ROI.size
-                            new_size = (cur_size[0]//4, cur_size[1]//4)
+                            new_size = (cur_size[0]//scale, cur_size[1]//scale)
                             small_img = ROI.resize(new_size, resample=PIL.Image.BICUBIC)
                             big_img = small_img.resize(cur_size, resample=PIL.Image.BICUBIC)
                             small_img.save(lr_ROI_small_dir/save_fn.name)
                             big_img.save(lr_ROI_dir/save_fn.name)
+
+                    if not max_per_movie: img_max = None
+
+def czi_to_tiffs(czi_fn, hr_dir, lr_dir, channels=None, depths=None, max_scale=1.5, max_per_movie=True):
+    hr_dir, lr_dir = Path(hr_dir), Path(lr_dir)
+    hr_dir.mkdir(parents=True, exist_ok=True)
+    lr_dir.mkdir(parents=True, exist_ok=True)
+    with czifile.CziFile(czi_fn) as czi_f:
+        proc_axes, proc_shape = get_czi_shape_info(czi_f)
+        if channels is None: channels = proc_shape['C']
+        if depths is None: depths = proc_shape['Z']
+        times = proc_shape['T']
+        x,y = proc_shape['X'], proc_shape['Y']
+        data = czi_f.asarray()
+        for channel in range(channels):
+            for depth in range(depths):
+                img_max = None
+                for time_col in range(times):
+                    idx = build_index(proc_axes, {'T': time_col, 'C': channel, 'Z':depth, 'X':slice(0,x),'Y':slice(0,y)})
+                    img = data[idx].astype(np.float)
+                    save_fn = f'{czi_fn.stem}_{channel:02d}_{depth:03d}_{time_col:03d}.tif'
+                    if img_max is None: img_max = img.max() * max_scale
+                    img /= img_max
+                    if not max_per_movie: img_max = None
+                    pimg = PIL.Image.fromarray((img*255).astype(np.uint8), mode='L')
+                    pimg.save(hr_dir/save_fn)
+                    cur_size = pimg.size
+                    new_size = (cur_size[0]//4, cur_size[1]//4)
+                    small_img = pimg.resize(new_size, resample=PIL.Image.BICUBIC)
+                    small_img.save(lr_dir/save_fn)
---
+++ uri/scripts/datasets/movies_002.py
@@ -0,0 +1,69 @@
+from pathlib import Path
+import shutil
+import random
+import os
+import helpers
+from fastprogress import progress_bar
+
+data_path = Path('/scratch/bpho')
+sources = data_path/'datasources'
+src_1 = sources/'MitoTracker_Red_FM_movie_data'
+
+datasets = data_path/'datasets'
+movies_002 = datasets/'movies_002'
+train = movies_002/'train'
+valid = movies_002/'valid'
+test = movies_002/'test'
+
+if movies_002.exists(): shutil.rmtree(movies_002)
+for fld in [train, valid, test]: fld.mkdir(parents=True, mode=0o777)
+
+test_mito = list(src_1.glob('*230*.czi'))
+train_mito = list(src_1.glob('*920*.czi'))
+
+valid_pct = 0.20
+train_files = []
+valid_files = []
+test_files = []
+
+random.seed(42)
+for lst in [train_mito]:
+    lst.sort()
+    random.shuffle(lst)
+    split_idx = int(valid_pct * len(lst))
+    valid_files += lst[-split_idx:]
+    train_files += lst[:-split_idx]
+
+test_files = test_mito
+
+for x in train_files: shutil.copy(x, train/x.name)
+for x in valid_files: shutil.copy(x, valid/x.name)
+for x in test_files: shutil.copy(x, test/x.name)
+
+train_files = progress_bar(list(train.iterdir()))
+valid_files = progress_bar(list(valid.iterdir()))
+
+def save_tiles(tile_size):
+    print(f'\n\nsave {tile_size} tiles')
+    hr_ROI = movies_002/f'roi_hr_{tile_size}'
+    lr_ROI = movies_002/f'roi_lr_{tile_size}'
+    lr_ROI_small = movies_002/f'roi_lr_small_{tile_size}'
+
+    for fn in train_files:
+        helpers.czi_to_tiles(fn, hr_ROI/'train', lr_ROI/'train', lr_ROI_small/'train', 
+        size=tile_size, channels=1, max_scale=1.05, max_per_movie=False)
+    for fn in valid_files:
+        helpers.czi_to_tiles(fn, hr_ROI/'valid', lr_ROI/'valid', lr_ROI_small/'valid', 
+        size=tile_size, channels=1, max_scale=1.05, max_per_movie=False)
+
+img_hr = movies_002/'img_hr'
+img_lr = movies_002/'img_lr'
+print('save tiffs')
+for fn in train_files: helpers.czi_to_tiffs(fn,img_hr/'train',img_lr/'train', max_scale=1.05, max_per_movie=False)
+for fn in valid_files: helpers.czi_to_tiffs(fn,img_hr/'valid',img_lr/'valid', max_scale=1.05, max_per_movie=False)
+
+for sz in [64,128,256,512]: save_tiles(sz)
+
+
+helpers.chmod_all_readwrite(movies_002)
+print('\n\ndone')

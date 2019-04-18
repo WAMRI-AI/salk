f576f3f30eb639999d64126a5c4b2e8ffec19a99
Fred Monroe <fred@313v.com>
2019-04-17 21:46:11 -0400

    working on multitile blend

--- uri/paper/bpho/utils.py
+++ uri/paper/bpho/utils.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import torch
 import math
 from .multi import MultiImage
+import pyvips
 
 __all__ = ['generate_movies', 'generate_tifs']
 
@@ -65,6 +66,133 @@ def unet_multi_image_from_tiles(learn, in_img, tile_sz=128, scale=4, wsize=3):
                     out_y_end] = out_tile.data[:, in_x_start:in_x_end,
                                                in_y_start:in_y_end]
     return out_img
+
+
+# map vips formats to np dtypes
+format_to_dtype = {
+    'uchar': np.uint8,
+    'char': np.int8,
+    'ushort': np.uint16,
+    'short': np.int16,
+    'uint': np.uint32,
+    'int': np.int32,
+    'float': np.float32,
+    'double': np.float64,
+    'complex': np.complex64,
+    'dpcomplex': np.complex128,
+}
+
+# map np dtypes to vips
+dtype_to_format = {
+    'uint8': 'uchar',
+    'int8': 'char',
+    'uint16': 'ushort',
+    'int16': 'short',
+    'uint32': 'uint',
+    'int32': 'int',
+    'float32': 'float',
+    'float64': 'double',
+    'complex64': 'complex',
+    'complex128': 'dpcomplex',
+}
+
+
+# numpy array to vips image
+def numpy2vips(a):
+    height, width, bands = a.shape
+    linear = a.reshape(width * height * bands)
+    vi = pyvips.Image.new_from_memory(linear.data, width, height, bands,
+                                      dtype_to_format[str(a.dtype)])
+    return vi
+
+def tile2vips(t):
+    a = t.cpu().detach().numpy()
+    c,h,w = a.shape
+    return numpy2vips(a.reshape((h,w,c)))
+
+# vips image to numpy array
+def vips2numpy(vi):
+    return np.ndarray(buffer=vi.write_to_memory(),
+                      dtype=format_to_dtype[vi.format],
+                      shape=[vi.height, vi.width, vi.bands])
+
+def vip2tensor(vi):
+    a = vips2numpy(vi)
+    return tensor(a.transpose(2, 0, 1))
+
+def unet_image_from_tiles_blend(learn, in_img, tile_sz=128, scale=4, overlap=80):
+    #  set_trace()
+    cur_size = in_img.shape[1:3]
+    c = in_img.shape[0]
+    new_size = (cur_size[0] * scale, cur_size[1] * scale)
+    h, w = cur_size
+
+    in_tile = torch.zeros((c, tile_sz // scale, tile_sz // scale))
+    #out_img = torch.zeros((1, w * scale, h * scale))
+    tile_sz //= scale
+    overlap //= scale
+    over_sz = tile_sz - overlap
+    tiles = []
+
+    out_img = None
+    tiles_across = math.ceil(w/over_sz)
+    tiles_down = math.ceil(h/over_sz)
+
+    for y_tile in range(tiles_down):
+        row_img = None
+        y_start = y_tile * over_sz
+        y_end = min(y_start + tile_sz, h)
+
+        out_y_start = y_start * scale
+        out_y_end = y_end * scale 
+
+        in_y_start = 0
+        in_y_end = (y_end - y_start) * scale
+
+        for x_tile in range(tiles_across):
+            x_start = x_tile * over_sz
+            x_end = min(x_start + tile_sz, w)
+
+            out_x_start = x_start * scale
+            out_x_end = x_end * scale 
+
+            in_x_start = 0
+            in_x_end = (x_end - x_start) * scale
+
+            in_tile[:, 0:(y_end - y_start), 0:(x_end - x_start)] = tensor(in_img[:, y_start:y_end, x_start:x_end])
+            img = Image(tensor(npzoom(in_tile[0], scale, order=1)[None]))
+            out_tile, _, _ = learn.predict(img)
+            tile_data = out_tile.data[:, in_y_start:in_y_end, in_x_start:in_x_end]
+            tile = tile2vips(tile_data)
+            #  print(tile.height, tile.width, in_y_end, in_x_end)
+            tiles.append(tile)
+
+    spacing = over_sz * scale # - 1
+    print(len(tiles), tiles_across, tiles_down, spacing)
+
+    #  base = pyvips.Image.black(new_size[1], new_size[0], bands=1)
+    for y in range(tiles_down):
+        row_img = None
+        for x in range(tiles_across):
+            idx = y*tiles_across + x
+            #  print('idx:', idx)
+            a_tile = tiles[idx]
+            if row_img is None: row_img = a_tile
+            else: 
+                row_img = a_tile.merge(row_img, 'horizontal', x*spacing, 0) #:w, mblend=spacing//2).copy_memory()
+                #  print(x,y, x*spacing, x*spacing + spacing//2)
+                #  row_img = row_img.mosaic(a_tile, 'horizontal', x*spacing,0, x*spacing + spacing//2, 0, harea=overlap//2, hwindow=3)
+                #  print('coo')
+            #  print('row: ', row_img)
+        if out_img is None: out_img = row_img
+        else:
+            out_img = row_img.merge(out_img, 'vertical', 0, y*spacing, mblend=spacing//2)
+        #  print('out: ', out_img)
+            #  print(row_img, out_img, x, y)
+            #  out_img = out_img.mosaic(row_img, 'vertical', 0, y*spacing, 0, y*spacing + spacing//2, harea=overlap//2, hwindow=3)
+    #  out_img = pyvips.Image.arrayjoin(tiles, across=tiles_across, halign='low', valign='low', hspacing=spacing, vspacing=spacing, shim=-3) #, shim=10, halign='low', valign='low', hspacing=spacing, vspacing=spacing)
+     
+    return vip2tensor(out_img)
 
 
 def unet_image_from_tiles(learn, in_img, tile_sz=128, scale=4):
@@ -293,7 +421,7 @@ def tif_predict_images(learn,
         img = img_data[t].copy()
         img /= img_max
 
-        out_img = unet_image_from_tiles(learn, img[None], tile_sz=size)
+        out_img = unet_image_from_tiles_blend(learn, img[None], tile_sz=size)
         pred = (out_img * 255).cpu().numpy().astype(np.uint8)
         preds.append(pred)
         orig = (img[None] * 255).astype(np.uint8)
@@ -306,7 +434,7 @@ def tif_predict_images(learn,
         imageio.mimwrite(orig_out, all_y, bigtiff=True)
 
 
-def czi_predict_images(learn
+def czi_predict_images(learn,
                        czi_in,
                        dest,
                        category,
@@ -361,7 +489,7 @@ def czi_predict_images(learn
                         img = data[idx].copy()
                         img /= img_max
 
-                        out_img = unet_image_from_tiles(learn,
+                        out_img = unet_image_from_tiles_blend(learn,
                                                         img[None],
                                                         tile_sz=size)
                         pred = (out_img * 255).cpu().numpy().astype(np.uint8)
@@ -381,22 +509,22 @@ def czi_predict_images(learn
 def generate_tifs(src, dest, learn, size, tag=None, max_imgs=None):
     for fn in progress_bar(src):
         category = fn.parts[-3]
-        try:
-            if fn.suffix == '.czi':
-                czi_predict_images(learn,
-                                   fn,
-                                   dest,
-                                   category,
-                                   size=size,
-                                   tag=tag,
-                                   max_imgs=max_imgs)
-            elif fn.suffix == '.tif':
-                tif_predict_images(learn,
-                                   fn,
-                                   dest,
-                                   category,
-                                   size=size,
-                                   tag=tag,
-                                   max_imgs=max_imgs)
-        except:
-            print(f'exception with {fn.stem}')
+        #  try:
+        if fn.suffix == '.czi':
+            czi_predict_images(learn,
+                               fn,
+                               dest,
+                               category,
+                               size=size,
+                               tag=tag,
+                               max_imgs=max_imgs)
+        elif fn.suffix == '.tif':
+            tif_predict_images(learn,
+                               fn,
+                               dest,
+                               category,
+                               size=size,
+                               tag=tag,
+                               max_imgs=max_imgs)
+        #  except:
+        #      print(f'exception with {fn.stem}')

12efd8509a46ea208482f94e531c58f1c283b539
Fred Monroe <fred@313v.com>
2019-05-03 22:27:54 -0400

    gen multi data

--- uri/paper/multi_from_info.py
+++ uri/paper/multi_from_info.py
@@ -19,11 +19,14 @@ PIL.Image.MAX_IMAGE_PIXELS = 99999999999999
 def default_crap(img, scale=4, upsample=True, pscale=12, gscale=0.0001):
     from skimage.transform import rescale
 
+
+    # set_trace()
     # pull pixel data from PIL image
     x = np.array(img)
     multichannel = len(x.shape) > 2
 
     # remember the range of the original image
+    dmax = np.iinfo(x.dtype).max
     img_max = x.max()
 
     # normalize 0-1.0
@@ -46,6 +49,8 @@ def default_crap(img, scale=4, upsample=True, pscale=12, gscale=0.0001):
 
     if upsample: x = rescale(x, scale=scale, order=0, multichannel=multichannel)
 
+    x /= dmax
+    x *= np.iinfo(np.uint8).max
     return PIL.Image.fromarray(x.astype(np.uint8))
 
 def need_cache_flush(tile_stats, last_stats):
@@ -56,96 +61,89 @@ def need_cache_flush(tile_stats, last_stats):
     #         return True
     return False
 
-def get_tile_puller(tile_stat, crap_func):
+def get_tile_puller(tile_stat, crap_func, n_frames):
     fn = tile_stat['fn']
     ftype = tile_stat['ftype']
-
+    nz = tile_stat['nz']
+    nt = tile_stat['nt']
+    half_frames = n_frames // 2
 
     if ftype == 'czi':
         img_f = czifile.CziFile(fn)
         proc_axes, proc_shape = get_czi_shape_info(img_f)
-        img_data = img_f.asarray().astype(np.float32)
-        img_max = img_data.max()
-        thresh = 0.01
-        thresh_pct = (img_data.mean() > 1) * 0.5
+        img_data = img_f.asarray()
+        img_max = img_data.max() # np.iinfo(img_data.dtype).max
+        img_data = img_data.astype(np.float32)
 
         def czi_get(istat):
-            n_frames = istat['n_frames']
-            nt = istat['nt']
-            nz = istat['nz']
-            c = istat['c']
-            z = istat['z']
-            t = istat['t']
-            x = istat['x']
-            y = istat['y']
-
-            if nz > 1:
-                offset = n_frames // 2
-                min_z = z-offset
-                max_z = z+offset+1
-                if max_z > nz:
-                    max_z = nz
-                    min_z = max_z-n_frames-1
-                elif min_z < 0:
-                    min_z = 0
-                    max_z = n_frames + 1
-                z = slice(min_z, max_z)
+            c,z,t,x,y,mi,ma = [istat[fld] for fld in ['c','z','t','x','y','mi','ma']]
+            if nz > n_frames:
+                idx = build_index(
+                    proc_axes, {
+                        'T': t,
+                        'C': c,
+                        'Z': slice(z-half_frames, z+half_frames+1),
+                        'X': slice(0, x),
+                        'Y': slice(0, y)
+                    })
+                type_frames = 'Z'
             else:
-                offset = n_frames // 2
-                min_t = t-offset
-                max_t = t+offset+1
-                if max_t > nt:
-                    max_t = nt
-                    min_t = max_t-n_frames-1
-                elif min_t < 0:
-                    min_t = 0
-                    max_t = n_frames + 1
-                t = slice(min_t, max_t)
+                idx = build_index(
+                    proc_axes, {
+                        'T': slice(t-half_frames, t+half_frames+1),
+                        'C': c,
+                        'Z': z,
+                        'X': slice(0, x),
+                        'Y': slice(0, y)
+                    })
+                type_frames = 'T'
 
-            idx = build_index(
-                proc_axes, {
-                    'T': t,
-                    'C': c,
-                    'Z': z,
-                    'X': slice(0, x),
-                    'Y': slice(0, y)
-                })
-            img = img_data[idx].copy().astype(np.float32)
-            img /= img_max
-            return img
+            img = img_data[idx].copy()
+            # img /= img_max
+            eps = 1e-20
+            img = (img - mi) / (ma - mi + eps)
+            img = img.clip(0,1.)
+            # return img.clip(0.,1.)
+            return img, type_frames
+
         img_get = czi_get
         img_get._to_close = img_f
     else:
         pil_img = PIL.Image.open(fn)
-        n_frames = istat['n_frames']
-        nz = istat['nz']
-        z = istat['z']
-        thresh = 0.01
-        thresh_pct = (img_data.mean() > 1) * 0.5
+        n_frames = pil_img.n_frames
 
-        offset = n_frames // 2
-        min_z = z-offset
-        max_z = z+offset+1
-        if max_z > nz:
-            max_z = nz
-            min_z = max_z-n_frames-1
-        elif min_z < 0:
-            min_z = 0
-            max_z = n_frames + 1
-        z = slice(min_z, max_z)
+        img = np.array(pil_img)
+        if len(img.shape) > 2: img = img[:,:,0]
+        img_max = img.max() # np.iinfo(img.dtype).max
+        img = img.astype(np.float32) / img_max
+
 
         def pil_get(istat):
-            print('fix z fetch')
-            set_trace()
-            pil_img.seek(istat['z'])
-            pil_img.load()
-            img = np.array(pil_img).astype(np.float32)
-            img /= img.max()
-            return img
-        img_get = pil_get
-        img_get._to_close = img_f
+            type_frams = 'Z'
+            c,z,t,x,y,mi,ma = [istat[fld] for fld in ['c','z','t','x','y','mi','ma']]
+            start_z, end_z = z - half_frames, z + half_framesi + 1
 
-    def puller(istat, tile_folder, crap_folder, close_me=False):
+            img_array = []
+            for iz in range(start_z, end_z):
+                pil_img.seek(z)
+                pil_img.load()
+                img = np.array(pil_img)
+                if len(img.shape) > 2: img = img[:,:,0]
+                img_array.append(img.copy())
+
+            # img_max = img.max()
+            # img = img.astype(np.float32) / img_max
+            img = np.stack(img_array)
+            img = img.astype(np.float32)
+            eps = 1e-20
+            img = (img - mi) / (ma - mi + eps)
+            img = img.clip(0,1.)
+            return img, type_frames
+
+        img_get = pil_get
+        img_get._to_close = pil_img
+
+    def puller(istat, z_tile_folder, t_tile_folder, z_crap_folder, t_crap_folder, close_me=False):
         if close_me:
             img_get._to_close.close()
             return None
@@ -153,14 +151,33 @@ def get_tile_puller(tile_stat, crap_func):
         id = istat['index']
         fn = Path(istat['fn'])
         tile_sz = istat['tile_sz']
+        c,z,t,x,y,mi,ma = [istat[fld] for fld in ['c','z','t','x','y','mi','ma']]
 
-        set_trace()
-        img_data = (255.0 * img_get(istat)).astype(np.uint8)
-        crop_img, box = draw_random_tile(img_data, istat['tile_sz'], thresh, thresh_pct)
+        raw_data, type_frames = img_get(istat)
+        if type_frames == 'Z':
+            tile_folder = z_tile_folder
+            crap_folder = z_crap_folder
+        else:
+            assert(type_frames == 'T')
+            tile_folder = t_tile_folder
+            crap_folder = t_crap_folder
+
+        img_data = (np.iinfo(np.uint8).max * raw_data).astype(np.uint8)
+
+        thresh = np.percentile(img_data, 2)
+        thresh_pct = (img_data > thresh).mean() * 0.75
+
+        mid_frame = n_frames // 2
+        crop_img, box = draw_random_tile(img_data[mid_frame], istat['tile_sz'], thresh, thresh_pct)
         crop_img.save(tile_folder/f'{id:06d}_{fn.stem}.tif')
         if crap_func and crap_folder:
-            crap_img = crap_func(crop_img)
-            crap_img.save(crap_folder/f'{id:06d}_{fn.stem}.tif')
+            crap_data = []
+            for i in range(n_frames):
+                frame_img = PIL.Image.fromarray(img_data[i, box[1]:box[3], box[0]:box[2]])
+                crap_frame = crap_func(crop_img)
+                crap_data.append(np.array(crap_frame))
+            multi_array = np.stack(crap_data)
+            np.save(crap_folder/f'{id:06d}_{fn.stem}.tif', multi_array)
 
         info = dict(istat)
         info['id'] = id
@@ -170,19 +187,28 @@ def get_tile_puller(tile_stat, crap_func):
 
     return puller
 
+def check_info(info, n_frames):
+    space = n_frames // 2
+    if info['nz'] > n_frames:
+        return (info['z'] > space) and (info['z'] < (info['nz']-space))
+    elif info['nt'] > n_frames:
+        return (info['t'] > space) and (info['t'] < (info['nt']-space))
+
+    return False
+
 @call_parse
 def main(out: Param("dataset folder", Path, required=True),
          info: Param('info file', Path, required=True),
          tile: Param('generated tile size', int, nargs='+', required=True),
-         frame: Param('num frames', int, nargs='+', required=True),
          n_train: Param('number of train tiles', int, required=True),
          n_valid: Param('number of validation tiles', int, required=True),
          crap_func: Param('crappifier name', object) = default_crap,
+         n_frames: Param('number of input frames', int) = 5,
          scale: Param('amount to scale', int) = 4,
          ftypes: Param('ftypes allowed e.g. - czi, tif', str, nargs='+') = None,
          not_unet: Param('unet style (down and back upsample)', action='store_true') = False,
          only: Param('limit to these categories', nargs='+') = None,
-         skip: Param("categories to skip", str, nargs='+') = ['random', 'centrioles','ArgoSIMDL', 'neurons', 'fixed_neurons'],
+         skip: Param("categories to skip", str, nargs='+') = ['random', 'ArgoSIMDL'],
          clean: Param("wipe existing data first", action='store_true') = False):
     "generate tiles from source tiffs"
     is_unet = not not_unet
@@ -199,10 +225,14 @@ def main(out: Param("dataset folder", Path, required=True),
         shutil.rmtree(out)
 
     info = pd.read_csv(info)
-    info = info.loc[info.multi == 1]
+
     if ftypes: info = info.loc[info.ftype.isin(ftypes)]
     if only: info = info.loc[info.category.isin(only)]
     elif skip: info = info.loc[~info.category.isin(skip)]
+
+    # filter to multi
+    max_frames = info[['nt','nz']].apply(max, axis=1)
+    info = info.loc[max_frames > n_frames]
 
     tile_infos = []
 
@@ -214,13 +244,14 @@ def main(out: Param("dataset folder", Path, required=True),
         for i in range(n_samples):
             category, cat_df = random.choice(categories)
             fn, item_df = random.choice(files_by_category[category])
-            item_info = item_df.loc[random.choice(item_df.index)]
+            legal_choices = [item_info for ix, item_info in item_df.iterrows() if check_info(item_info, n_frames)]
+            if not legal_choices:
+                set_trace()
+            item_info = random.choice(legal_choices)
             for tile_sz in tile:
-                for n_frames in frame:
-                    item_d = dict(item_info)
-                    item_d['tile_sz'] = tile_sz
-                    item_d['n_frames'] = n_frames
-                    tile_infos.append(item_d)
+                item_d = dict(item_info)
+                item_d['tile_sz'] = tile_sz
+                tile_infos.append(item_d)
 
     tile_info_df = pd.DataFrame(tile_infos).reset_index()
     print('num tile pulls:', len(tile_infos))
@@ -239,27 +270,24 @@ def main(out: Param("dataset folder", Path, required=True),
                 mode = tile_stat['dsplit']
                 category = tile_stat['category']
                 tile_sz = tile_stat['tile_sz']
-                n_frames = tile_stat['n_frames']
+                z_tile_folder = ensure_folder(out / f'hr_t_{tile_sz}_mz_{n_frames}' / mode / category)
+                t_tile_folder = ensure_folder(out / f'hr_t_{tile_sz}_mt_{n_frames}' / mode / category)
+                if crap_func:
+                    z_crap_folder = ensure_folder(out / f'lr{up}_t_{tile_sz}_mz_{n_frames}' / mode / category)
+                    t_crap_folder = ensure_folder(out / f'lr{up}_t_{tile_sz}_mt_{n_frames}' / mode / category)
+                else: crap_folder = None
 
-                if tile_stat['z'] > 1 or tile_stat['t'] > 1:
-                    ft = 'z' if tile_stat['z'] > 1 else 't'
-                    tile_folder = ensure_folder(out / f'hr_t_{tile_sz}_f{ft}_{n_frames}' / mode / category)
-                    if crap_func: crap_folder = ensure_folder(out / f'lr{up}_t_{tile_sz}_f{ft}_{n_frames}' / mode / category)
-                    else: crap_folder = None
-
-                    if need_cache_flush(tile_stat, last_stat):
-                        if tile_puller:
-                            tile_puller(None, None, None, close_me=True)
-                        last_stat = tile_stat.copy()
-                        tile_sz = tile_stat['tile_sz']
-                        tile_puller = get_tile_puller(tile_stat, crap_func)
-
-                    tile_pull_info.append(tile_puller(tile_stat, tile_folder, crap_folder))
+                if need_cache_flush(tile_stat, last_stat):
+                    if tile_puller:
+                        tile_puller(None, None, None, close_me=True)
+                    last_stat = tile_stat.copy()
+                    tile_sz = tile_stat['tile_sz']
+                    tile_puller = get_tile_puller(tile_stat, crap_func, n_frames)
+                tile_pull_info.append(tile_puller(tile_stat, z_tile_folder, t_tile_folder, z_crap_folder, t_crap_folder))
             except MemoryError as error:
                 # some files are too big to read
                 fn = Path(tile_stat['fn'])
                 print(f'too big: {fn.stem}')
 
-    pd.DataFrame(tile_pull_info).to_csv(out/'multitiles.csv', index = False)
-
+    pd.DataFrame(tile_pull_info).to_csv(out/'tiles.csv', index = False)
 

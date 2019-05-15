4ba0a113083f01ce20ce0dd54b4809371ae76e70
Fred Monroe <fred@313v.com>
2019-05-15 00:55:25 -0400

    del obsolete multi_from_info and make new_train_single replace train_single

--- uri/paper/train_single.py
+++ uri/paper/train_single.py
@@ -1,23 +1,20 @@
 
 import yaml
-yaml.warnings({'YAMLLoadWarning': False})
 from fastai.script import *
 from fastai.vision import *
 from fastai.callbacks import *
 from fastai.distributed import *
-from fastai.vision.models.xresnet import *
 from fastai.vision.models.unet import DynamicUnet
 from skimage.util import random_noise
 from skimage import filters
 from bpho import *
-
+from bpho.resnet import *
 
 torch.backends.cudnn.benchmark = True
 
 def get_src(x_data, y_data):
     def map_to_hr(x):
-        hr_name = x.relative_to(x_data)
-        return y_data/hr_name
+        return y_data/x.relative_to(x_data)
 
     src = (ImageImageList
             .from_folder(x_data, convert_mode='L')
@@ -25,40 +22,37 @@ def get_src(x_data, y_data):
             .label_from_func(map_to_hr, convert_mode='L'))
     return src
 
-def get_data(bs, size, x_data, y_data, max_zoom=1.1):
+
+def get_data(bs, size, x_data, y_data,
+             max_rotate=10.,
+             min_zoom=1., max_zoom=1.1,
+             use_cutout=False,
+             use_noise=True,
+             scale=4,
+             xtra_tfms=None,
+             **kwargs):
     src = get_src(x_data, y_data)
-    x_tfms, y_tfms = get_xy_transforms()
+    x_tfms, y_tfms = get_xy_transforms(
+                          max_rotate=max_rotate,
+                          min_zoom=min_zoom, max_zoom=max_zoom,
+                          use_cutout=use_cutout,
+                          use_noise=use_noise,
+                          xtra_tfms = xtra_tfms)
+    x_size = size // scale
     data = (src
-            .transform(x_tfms, size=size)
+            .transform(x_tfms, size=x_size)
             .transform_y(y_tfms, size=size)
-            .databunch(bs=bs))
+            .databunch(bs=bs, **kwargs).normalize(do_y=True))
     data.c = 3
     return data
 
 
-def do_fit(learn, save_name, lrs=slice(1e-3), pct_start=0.9, cycle_len=10):
-    learn.to_fp16().fit_one_cycle(cycle_len, lrs, pct_start=pct_start)
-    learn.save(save_name)
-    print(f'saved: {save_name}')
-    #num_rows = min(learn.data.batch_size, 3)
-    #learn.to_fp32().show_results(rows=num_rows, imgsize=5)
-
-
-def get_model(in_c, out_c, arch):
-    body = nn.Sequential(*list(arch(c_in=in_c).children())[:-2])
-    model = DynamicUnet(
-        body, n_classes=out_c,
-        blur=True, blur_final=True,
-        self_attention=True, norm_type=NormType.Weight,
-        last_cross=True, bottle=True
-    )
-    return model
 
 
 @call_parse
 def main(
         gpu: Param("GPU to run on", str)=None,
-        arch: Param("encode architecture", str) = 'xresnet34',
+        arch: Param("encode architecture", str) = 'wnresnet34',
         bs: Param("batch size per gpu", int) = 8,
         lr: Param("learning rate", float) = 1e-4,
         size: Param("img size", int) = 256,
@@ -66,7 +60,19 @@ def main(
         load_name: Param("load model name", str) = None,
         save_name: Param("model save name", str) = 'combo',
         datasetname: Param('dataset name', str) = 'tiles_002',
-        tile_sz: Param('tile_sz', int) = 512,
+        tile_sz: Param('tile_sz', int) = 256,
+        attn: Param('self attention', action='store_true')=True,
+        blur: Param('upsample blur', action='store_true')=True,
+        final_blur: Param('final upsample blur', action='store_true')=False,
+        bottle: Param('bottleneck', action='store_true')=True,
+        cutout: Param('bottleneck', action='store_true')=False,
+        rrdb: Param('use RRDB_Net', action='store_true')=False,
+        nf: Param('rrdb nf', int) = 32,
+        nb: Param('rrdb nb', int) = 32,
+        gcval: Param('rrdb gc', int) = 32,
+        clip_grad: Param('gradient clipping', float) = None,
+        loss_scale: Param('loss scale', float) = None,
+        feat_loss: Param('bottleneck', action='store_true')=False
 ):
     data_path = Path('.')
     datasets = data_path/'datasets'
@@ -76,12 +82,12 @@ def main(
 
     if tile_sz is None:
         hr_tifs = dataset/f'hr'
-        lrup_tifs = dataset/f'lrup'
+        lr_tifs = dataset/f'lr'
     else:
         hr_tifs = dataset/f'hr_t_{tile_sz:d}'
-        lrup_tifs = dataset/f'lrup_t_{tile_sz:d}'
+        lr_tifs = dataset/f'lr_t_{tile_sz:d}'
 
-    print(datasets, dataset, hr_tifs, lrup_tifs)
+    print(datasets, dataset, hr_tifs)
 
     model_dir = 'models'
 
@@ -89,20 +95,40 @@ def main(
     print('on gpu: ', gpu)
     n_gpus = num_distrib()
 
-    loss = F.mse_loss
+    loss = get_feat_loss() if feat_loss else F.l1_loss
     metrics = sr_metrics
 
-    bs = min(bs, bs * n_gpus)
+    bs = max(bs, bs * n_gpus)
     size = size
     arch = eval(arch)
 
-    data = get_data(bs, size, lrup_tifs, hr_tifs)
+    print('bs:', bs, 'size: ', size, 'ngpu:', n_gpus)
+    data = get_data(bs, size, lr_tifs, hr_tifs, max_zoom=4., use_cutout=cutout)
+    callback_fns = [partial(ReduceLROnPlateauCallback, patience=1)]
     if gpu == 0 or gpu is None:
-        callback_fns = []
+        if feat_loss:
+            callback_fns = [LossMetrics]
         callback_fns.append(partial(SaveModelCallback, name=f'{save_name}_best_{size}'))
-        learn = xres_unet_learner(data, arch, path=Path('.'), loss_func=loss, metrics=metrics, model_dir=model_dir, callback_fns=callback_fns)
+
+
+    if rrdb:
+        rrdb_args = {
+            'nf': nf,
+            'nb': nb,
+            'gcval': gcval,
+            'upscale': 4
+        }
+        learn = rrdb_learner(data, rrdb_args=rrdb_args, path=Path('.'),
+                             loss_func=loss, metrics=metrics, model_dir=model_dir, callback_fns=callback_fns)
     else:
-        learn = xres_unet_learner(data, arch, path=Path('.'), loss_func=loss, metrics=metrics, model_dir=model_dir)
+        wnres_args = {
+            'blur': blur,
+            'blur_final': final_blur,
+            'bottle': bottle,
+            'self_attention': attn
+        }
+        learn = wnres_unet_learner(data, arch, wnres_args=wnres_args, path=Path('.'),
+                                   loss_func=loss, metrics=metrics, model_dir=model_dir, callback_fns=callback_fns)
     gc.collect()
 
     if load_name:
@@ -111,7 +137,12 @@ def main(
 
     if gpu is None: learn.model = nn.DataParallel(learn.model)
     else: learn.to_distributed(gpu)
-    learn = learn.to_fp16()
+    if not clip_grad is None:
+        learn = learn.clip_grad(clip_grad)
+    if not loss_scale is None:
+        learn = learn.to_fp16(loss_scale=loss_scale)
+    else:
+        learn = learn.to_fp16()
     learn.fit_one_cycle(cycles, lr)
 
     if gpu == 0 or gpu is None:
